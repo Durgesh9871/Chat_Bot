7f89fed695dc2f93a1d778284c57273cb7e32f13
Durgesh9871 <devisingh9871686098@gmail.com>
2023-02-21 10:04:50 +0530

    file

---
+++ my-app/src/Main_Home_Page/MainHomePage.js
@@ -0,0 +1,12 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+
+const MainHomePage = () => {
+  return (
+    <Box>
+      hello world
+    </Box>
+  )
+}
+
+export  {MainHomePage}
