9099a24deec3fe6e1a63199d7d4a66f9bb86afed
Durgesh9871 <devisingh9871686098@gmail.com>
2023-02-21 10:13:28 +0530

    file

---
+++ my-app/src/Main_Home_Page/MainHomePage.jsx
@@ -0,0 +1,12 @@
+import { Box, Img } from '@chakra-ui/react'
+import React from 'react'
+
+const MainHomePage = () => {
+  return (
+    <Box>
+     <Img src="https://img.freepik.com/free-photo/beautiful-scenery-phragmites-plants-by-sea-with-swimming-pelican-sunset_181624-37787.jpg" width="100%"></Img>
+    </Box>
+  )
+}
+
+export  {MainHomePage}
