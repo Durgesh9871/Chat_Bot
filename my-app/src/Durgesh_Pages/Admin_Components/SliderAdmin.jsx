92b6ab8b1873652002d178c953a3e9d252fffda5
Durgesh9871 <devisingh9871686098@gmail.com>
2023-02-24 02:06:14 +0530

    file

--- my-app/src/Durgesh_Pages/Admin_Components/SliderAdmin.jsx
+++ my-app/src/Durgesh_Pages/Admin_Components/SliderAdmin.jsx
@@ -1,26 +1,114 @@
-import React, { useState } from 'react';
-import './styles.css';
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { Text } from "@chakra-ui/react";
 
-function Sidebar() {
+const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const toggleSidebar = () => {
-    setIsOpen(!isOpen);
-  }
 
   return (
-    <div className="sidebar">
-      <div className="toggle-button" onClick={toggleSidebar}>
-        <i className={`fa fa-${isOpen ? 'times' : 'bars'}`}></i>
-      </div>
-      <ul className={`sidebar-menu ${isOpen ? 'open' : ''}`}>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Services</a></li>
-        <li><a href="#">Contact</a></li>
-      </ul>
-    </div>
+    <>
+     
+      <SidebarNav isOpen={true}>
+        <SidebarWrap>
+          <NavIcon to="#">
+             <Text>Dashboard</Text>
+          </NavIcon>
+          <SidebarLink to="/">Home</SidebarLink>
+          <SidebarLink to="/about">About</SidebarLink>
+          <SidebarLink to="/services">Services</SidebarLink>
+          <Dropdown>
+            <DropdownBtn>
+              Dropdown 
+            </DropdownBtn>
+            <DropdownContent>
+              <SidebarLink >Dropdown Item 1</SidebarLink>
+              <SidebarLink >Dropdown Item 2</SidebarLink>
+              <SidebarLink >Dropdown Item 3</SidebarLink>
+            </DropdownContent>
+          </Dropdown>
+          {/* <LoginLogoutBtn onClick={handleLogout}>Logout</LoginLogoutBtn> */}
+        </SidebarWrap>
+      </SidebarNav>
+    </>
   );
-}
+};
 
-export default Sidebar;
+export {Sidebar}
+
+
+
+const NavIcon = styled(Link)`
+  margin-left: 2rem;
+  font-size: 2rem;
+  height: 80px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  color: #fff;
+`;
+
+const SidebarNav = styled.nav`
+  background: #171923;
+  width: 250px;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: ${({ isOpen }) => (isOpen ? "0" : "-100%")};
+  transition: 350ms;
+  z-index: 10;
+`;
+
+const SidebarWrap = styled.div`
+  width: 100%;
+`;
+
+const SidebarLink = styled.div`
+  display: flex;
+  color: #fff;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+  list-style: none;
+  height: 60px;
+  text-decoration: none;
+  font-size: 18px;
+  &:hover {
+    background-color: #04aa6d;
+    border-left: 4px solid #fff;
+    cursor: pointer;
+  }
+`;
+
+const Dropdown = styled.div`
+  display: block;
+  position: relative;
+  color: #fff;
+`;
+
+const DropdownBtn = styled.div`
+  display: flex;
+  color: #fff;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+  list-style: none;
+  height: 60px;
+  text-decoration: none;
+  font-size: 18px;
+  &:hover {
+    background-color: #04aa6d;
+    border-left:`
+
+
+  const DropdownContent = styled.div`
+  display: none;
+  position: absolute;
+  background-color: #171923;
+  min-width: 160px;
+  z-index: 1;
+`;
--- my-app/src/Durgesh_Pages/Pages/MainAdminPage.jsx
+++ my-app/src/Durgesh_Pages/Pages/MainAdminPage.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Box } from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
-import Sidebar from '../Admin_Components/SliderAdmin'
+import {Sidebar} from "../Admin_Components/SliderAdmin"
 
 const MainAdminPage = () => {
   return (
     <div>
-       <Box border="2px solid red" width="100%" height="10vh" display="flex" justifyContent="space-evenly" >
+       {/* <Box border="2px solid red" width="100%" height="10vh" display="flex" justifyContent="space-evenly" >
      <Link to="/"> <Box>Home Page</Box> </Link>
      <Link to="/adminPage"> <Box>Admin Page</Box> </Link>
-      </Box>
+      </Box> */}
 
   {/*  Main Admin Page Started from here ---------- */}
    <Box>
