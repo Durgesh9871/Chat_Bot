809d8ef7548dd33ba98a238d1f03280b81cef910
Durgesh9871 <devisingh9871686098@gmail.com>
2023-02-23 19:30:40 +0530

    file

--- my-app/src/Durgesh_Pages/Pages/MainHomePage.jsx
+++ my-app/src/Durgesh_Pages/Pages/MainHomePage.jsx
@@ -28,9 +28,9 @@ const MainHomePage = () => {
 
       {/*  Main Code started from here ---------------------- */}
          
-         <Box height="200vh" border="2px solid red">
+         <Box height="600vh" border="2px solid red">
             <Box id='mainImageNav'>
-              <Button color="#4C4C4C" border="1px solid #4C4C4C" borderRadius="4px" p="0px 25px" position="absolute" bottom={{base:"3%", sm: "4%", md: "8%", lg: "8%",xl: "45px",'2xl': "45px"}} left={{base:"10%", sm: "8.4%", md: "5.5%", lg: "5.5%",xl: "85px",'2xl': "85px"}}>SHOP MEN</Button>
+              <Button color="#4C4C4C" border="1px solid #4C4C4C" borderRadius="2px" p="0px 25px" position="absolute" bottom={{base:"3%", sm: "4%", md: "8%", lg: "8%",xl: "45px",'2xl': "45px"}} left={{base:"10%", sm: "8.4%", md: "5.5%", lg: "5.5%",xl: "85px",'2xl': "85px"}}>SHOP MEN</Button>
             </Box>
           
           {/*  After MAIN IMAGE HEADING ----------------- */}
@@ -41,7 +41,7 @@ performance in unpredictable conditions.</Text>
             </Box>
 
             {/*After heading Main Images -------------------- */}
-            <Box border="2px solid red" height="auto" display="flex" justifyContent="space-evenly" flexDirection={{base:"column", sm: "row", md: "row", lg: "row",xl: "row",'2xl': "row"}} p="0px"   >
+            <Box border="2px  red" height="auto" display="flex" justifyContent="space-evenly" flexDirection={{base:"column", sm: "row", md: "row", lg: "row",xl: "row",'2xl': "row"}} p="0px"   >
 
               {
                 ImageBox.length > 0 && ImageBox.map((item,id)=>{
@@ -53,6 +53,15 @@ performance in unpredictable conditions.</Text>
                   )
                 })
               }
+
+            </Box>
+
+            {/*  Sale Box started from here ------------------- */} 
+
+            <Box border="2px solid red" height="auto"  textAlign="center" m="50px auto 0px auto" >
+              <Img src='https://github.com/Durgesh9871/ReadmeImages/blob/main/Fashion_Gallery/Sale_fashion_Galeery.png?raw=true' m="20px auto 0px auto"></Img>
+            
+              <Button color="white" mt="14px" border="1px solid #333333"  backgroundColor="#333333" borderRadius="1px" p="0px 27px" >SHOP MEN</Button>
 
             </Box>
 
