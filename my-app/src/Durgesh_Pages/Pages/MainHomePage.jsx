bbce02b9ff8c0d22655248257354ebf8f4ddc421
Durgesh9871 <devisingh9871686098@gmail.com>
2023-02-23 17:46:36 +0530

    file

--- my-app/src/Durgesh_Pages/Pages/MainHomePage.jsx
+++ my-app/src/Durgesh_Pages/Pages/MainHomePage.jsx
@@ -17,8 +17,15 @@ const MainHomePage = () => {
          <Box >
             <Box id='mainImageNav'>
               <Button color="#4C4C4C" border="1px solid #4C4C4C" borderRadius="4px" p="0px 25px" position="absolute" bottom={{base:"3%", sm: "4%", md: "8%", lg: "8%",xl: "45px",'2xl': "45px"}} left={{base:"10%", sm: "8.4%", md: "5.5%", lg: "5.5%",xl: "85px",'2xl': "85px"}}>SHOP MEN</Button>
-
             </Box>
+          
+          {/*  After MAIN IMAGE HEADING ----------------- */}
+            <Box>
+              <Heading>Spring Adventures Are Calling!</Heading>
+              <Text>Outfit yourself with tech-enhanced essentials built for superior
+performance in unpredictable conditions.</Text>
+            </Box>
+
          </Box>
      
     </Box>
