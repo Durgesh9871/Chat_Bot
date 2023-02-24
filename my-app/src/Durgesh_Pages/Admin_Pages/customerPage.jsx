af9817b92b725f655fcf284f07325a0fa75308ed
Durgesh9871 <devisingh9871686098@gmail.com>
2023-02-24 12:03:59 +0530

    file

--- my-app/src/Durgesh_Pages/Admin_Pages/customerPage.jsx
+++ my-app/src/Durgesh_Pages/Admin_Pages/customerPage.jsx
@@ -6,6 +6,8 @@ const CustomerPage = () => {
   return (
     <Box>
        <Sidebar />
+       
+       {/*  Customer Page Started from here --------- */}
        <Box>
            <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae impedit harum porro iure temporibus possimus inventore! Illo eaque, sapiente quas dicta tempora, ducimus neque quia mollitia provident ex pariatur vitae?</Text>
        </Box>
