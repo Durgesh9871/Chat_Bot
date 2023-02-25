79fb4ad04090f7d962cc74f2ca4a4efb9c3bee4f
Durgesh9871 <devisingh9871686098@gmail.com>
2023-02-25 10:00:19 +0530

    file

--- my-app/src/Durgesh_Pages/Admin_Pages/DeletePage.jsx
+++ my-app/src/Durgesh_Pages/Admin_Pages/DeletePage.jsx
@@ -10,7 +10,21 @@ const DeletePage = () => {
 
        {/*  Delete Page Started from here ---------------------- */}
        <Box width="81%" border="1px red" height="auto" marginLeft="auto" >
-            jjcd
+            
+
+       <SimpleGrid columns={{base:1, sm: 1, md: 2, lg: 3,xl: 3,'2xl': 3,}} spacingY={10}  >
+        
+            {productArrayLaptop.length > 0 && productArrayLaptop.map((item)=>{
+              return (
+            <DisplayProductMainData key= {item.id} uniqueId={item.id}  id={item.id} src={item.Images} name={item.title} model={item.brand}    price={item.price}    review={item.rating}  realPrice={item.realPrice} isLaptopLoading={isLaptopLoading} mainImage={item.mainImage} />
+            
+              )
+            })}
+
+            
+        
+          </SimpleGrid>
+        
 
        </Box>
        {/* Delete box end --------- */}
