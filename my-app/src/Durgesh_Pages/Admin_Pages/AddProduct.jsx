427eac54321e1081e645231145acf2619f28a9a6
Durgesh9871 <devisingh9871686098@gmail.com>
2023-02-25 17:30:45 +0530

    file

--- my-app/src/Durgesh_Pages/Admin_Pages/AddProduct.jsx
+++ my-app/src/Durgesh_Pages/Admin_Pages/AddProduct.jsx
@@ -1,10 +1,75 @@
-import { Box } from '@chakra-ui/react'
+import { Box, FormControl, FormLabel, Heading, Input } from '@chakra-ui/react'
 import React from 'react'
+import { Sidebar } from '../Admin_Components/SliderAdmin'
 
 const AddProduct = () => {
   return (
     <Box>
-        hello
+        <Sidebar />
+        <Box background="#171923" height="100%">
+
+          {/* ADD PRODUCT FORM STARTED FROM HERE --------------------- */}
+          <Box width={{ base: "100%", sm: "100%", md: "100%", lg: "100%", xl: "81%", '2xl': "81%" }} border="1px   red" height="auto" marginLeft="auto"  p="2%" >
+          <Heading color="#fff" fontWeight="500">Add Products</Heading>
+
+           {/*  Form Starts from here ------ */}
+              <Box width="40%" border="1px  white" margin="auto" shadow="2xl" padding="30px">
+
+           <FormControl isRequired  color="#fff" mt="10px">
+         <FormLabel>Title</FormLabel>
+         <Input placeholder='Title of Product' />
+          </FormControl>
+           
+          <FormControl isRequired color="#fff" mt="20px" >
+         <FormLabel>Category</FormLabel>
+         <Input placeholder='Category of Product' />
+          </FormControl>
+
+          <FormControl isRequired color="#fff" mt="20px" >
+         <FormLabel>Brand</FormLabel>
+         <Input placeholder='Brand of Product' />
+          </FormControl>
+
+          <FormControl isRequired color="#fff" mt="20px" >
+         <FormLabel>Description</FormLabel>
+         <Input placeholder='Description about the Product' />
+          </FormControl>
+
+          <FormControl isRequired color="#fff" mt="20px" >
+         <FormLabel>Color</FormLabel>
+         <Input placeholder='Color of Product' />
+          </FormControl>
+
+          <FormControl isRequired color="#fff" mt="20px" >
+         <FormLabel>Actual Price</FormLabel>
+         <Input placeholder='Real Price of Product ' />
+          </FormControl>
+
+          <FormControl isRequired color="#fff" mt="20px" >
+         <FormLabel>Price</FormLabel>
+         <Input placeholder='Price after Discount' />
+          </FormControl>
+
+          <FormControl isRequired color="#fff" mt="20px" >
+         <FormLabel>Image</FormLabel>
+         <Input placeholder='Main Image of Product ' />
+          </FormControl>
+
+          <FormControl isRequired color="#fff" mt="20px" >
+         <FormLabel>Side Image</FormLabel>
+         <Input placeholder='Side Image of Product' />
+          </FormControl>
+
+          <FormControl isRequired color="#fff" mt="20px" >
+         <FormLabel>Back Image</FormLabel>
+         <Input placeholder='Back Image of Product ' />
+          </FormControl>
+
+          </Box>
+
+      </Box>
+        </Box>
+
     </Box>
   )
 }
--- my-app/src/Routes/Routes.jsx
+++ my-app/src/Routes/Routes.jsx
@@ -1,6 +1,7 @@
 
 import React from 'react'
 import {Routes , Route} from "react-router-dom"
+import { AddProduct } from '../Durgesh_Pages/Admin_Pages/AddProduct'
 import { CustomerPage } from '../Durgesh_Pages/Admin_Pages/customerPage'
 import { DeletePage } from '../Durgesh_Pages/Admin_Pages/DeletePage'
 import { MainAdminPage } from '../Durgesh_Pages/Pages/MainAdminPage'
@@ -14,6 +15,7 @@ const AllRoutes = () => {
       <Route path="/adminPage" element={<MainAdminPage />}></Route>
       <Route path="/customerPageAdmin" element={<CustomerPage />}></Route>
       <Route path="/deltePageAdmin" element={<DeletePage />}></Route>
+      <Route path="/addPageAdmin" element={<AddProduct />}></Route>
 
     </Routes>
   )
