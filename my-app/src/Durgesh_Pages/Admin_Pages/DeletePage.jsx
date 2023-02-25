31fd272843f5d50dca6e7f6a1086d942fc9b0af7
Durgesh9871 <devisingh9871686098@gmail.com>
2023-02-25 10:02:34 +0530

    file

--- my-app/src/Durgesh_Pages/Admin_Pages/DeletePage.jsx
+++ my-app/src/Durgesh_Pages/Admin_Pages/DeletePage.jsx
@@ -1,8 +1,24 @@
-import { Box } from '@chakra-ui/react'
-import React from 'react'
+import { Box, SimpleGrid } from '@chakra-ui/react'
+import React, { useEffect } from 'react'
+import { useSelector , useDispatch} from 'react-redux'
+import { getDataProduct } from '../../Redux/Laptop_reducer/action'
 import { Sidebar } from '../Admin_Components/SliderAdmin'
 
 const DeletePage = () => {
+
+
+    const {isLaptopError ,isLaptopLoading,productArrayLaptop} = useSelector((state) => {
+        return {
+          isLaptopLoading: state.LaptopReducer.isLaptopLoading ,
+          productArrayLaptop:state.LaptopReducer.productArrayLaptop ,
+          isLaptopError :state.LaptopReducer.isLaptopError ,
+        }
+    })   
+    const dispatch = useDispatch()
+    useEffect(()=>{
+         dispatch(getDataProduct)
+    },[])
+
   return (
     <Box>
        <Sidebar />
@@ -13,7 +29,7 @@ const DeletePage = () => {
             
 
        <SimpleGrid columns={{base:1, sm: 1, md: 2, lg: 3,xl: 3,'2xl': 3,}} spacingY={10}  >
-        
+
             {productArrayLaptop.length > 0 && productArrayLaptop.map((item)=>{
               return (
             <DisplayProductMainData key= {item.id} uniqueId={item.id}  id={item.id} src={item.Images} name={item.title} model={item.brand}    price={item.price}    review={item.rating}  realPrice={item.realPrice} isLaptopLoading={isLaptopLoading} mainImage={item.mainImage} />
